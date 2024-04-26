385d7f385c7fb5c5f31459409ee993f14c620975
Anamikaa18 <Anamikana18@gmail.com>
2024-04-26 21:13:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fifthmedplayr/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fifthmedplayr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView t1;
+    Button b1,b2;
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1=(TextView) findViewById(R.id.textView);
+        b1=(Button) findViewById(R.id.button);
+        b2=(Button) findViewById(R.id.button3);
+        mp=MediaPlayer.create(this,R.raw.j);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.pause();
+            }
+        });
+    }
+}
